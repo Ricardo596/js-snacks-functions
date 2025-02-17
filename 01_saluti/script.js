385c7db3ca34389a8fb3af1943c1affdebf0f863
Ricardo596 <ricardodiotti8@gmail.com>
2025-02-17 16:23:02 +0100

    Non so se va bene

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function ciao (Mario){
+    return "ciao" + Mario
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(ciao)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
