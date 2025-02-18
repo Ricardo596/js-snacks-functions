b0970ddd815ac18e4417b1c112536f6f9940730a
Ricardo596 <ricardodiotti8@gmail.com>
2025-02-18 09:56:07 +0100

    Correzione in classe

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function mapInitial(array){
+    const initials = []
+    for (let index = 0; index < array.length; index++) {
+        const thisElement = array[index];
+        initials.push(thisElement[0])
+    }
+    
 
+
+
+    return initials
+} 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const initials = mapInitial(names)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log(initials)
